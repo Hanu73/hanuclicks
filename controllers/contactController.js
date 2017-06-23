18959d2d2755320299c32f3549bb92b63ebd5589
hanu73 <saihanuman73@gmail.com>
2017-06-23 13:07:00 +0530

    added code to prevent cache

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,4 @@
-app.controller('contactController',function($scope){
+app.controller('contactController',function($scope,$http){
 
     $scope.stuff=[
         {
@@ -11,6 +11,20 @@ app.controller('contactController',function($scope){
             matter:'wassup'
         }
     ];
+
+   /* $scope.getLocation =function() {
+        $http({
+            method: "GET",
+            url: "https://ipinfo.io"
+        }).then(function mySuccess(data) {
+             data = JSON.stringify(data);
+             var x = data;
+             console.log(x)
+
+        }, function myError(data) {
+            alert("Error")
+        });
+    }*/
     $(document).on('click', '#content', function(e) {
         $('.navbar-collapse').collapse('hide');
        /* $(".navbar-main-collapse").removeClass("in",'300');*/
